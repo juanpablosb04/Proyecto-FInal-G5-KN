d9af2749161fe8314406a870e85d72a2044e5072
jason moya brenes <jason.moyabre.es@gmail.com>
2025-06-02 21:47:13 -0600

    plantilla profe

---
+++ ProyectoFinalKN/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace ProyectoFinalKN.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Principal()
+        {
+            return View();
+        }
+
+    }
+}
